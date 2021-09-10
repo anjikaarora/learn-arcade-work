1018699bd34919862638d23ccfe8cd0b1f2cc2c0
Anjika Arora <aarora@overlake.org>
2021-09-10 13:53:43 -0700

    Lab 1 Done

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,17 +1,1 @@
 print("Hello World!")
-print("I'm Bored :D")
-print("This \n is \n on \n separate \n lines \n wow!")
-print("\t tab function~")
-print("want to use quotes? \" use a backslash to do so!")
-print("I like chocolate.",end=" ")
-print(":)")
-print("""I"m printing
-on different
-lines now!           hehehe 
-
-:p""")
-print("""
-
-
-
-""")
